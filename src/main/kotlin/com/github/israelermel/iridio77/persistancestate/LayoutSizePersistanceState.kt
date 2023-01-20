1f3521afe124a145fc499b00ff1574cb4353bf79
israel ermel <israelermel@gmail.com>
2023-01-20 20:07:24 -0300

    melhorias na organização dos códigos

--- src/main/kotlin/com/github/israelermel/iridio77/persistancestate/LayoutSizePersistanceState.kt
+++ src/main/kotlin/com/github/israelermel/iridio77/persistancestate/LayoutSizePersistanceState.kt
@@ -1,7 +1,11 @@
 package com.github.israelermel.iridio77.persistancestate
 
 import com.github.israelermel.iridio77.ui.models.LayoutSizes
-import com.intellij.openapi.components.*
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 
 @Service
